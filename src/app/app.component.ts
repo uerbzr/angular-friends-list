7990cc0d4430a444c4bd83aef8777c171b464834
Lewis <ljoncb@gmail.com>
2023-06-20 17:49:23 +0100

    initial starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-friends-list';
+  newFriend: string | null = null;
+  favorite: string | null = null;
+
+  people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
+
+  addFriend() {
+    if (!this.newFriend) {
+      return;
+    }
+    this.people.push(this.newFriend);
+    this.newFriend = null;
+  }
 }
