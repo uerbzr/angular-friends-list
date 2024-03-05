6b7130f12ef8795079534b0bf8764f63ccf94994
uerbzr <nigel.sibbert@gmail.com>
2024-03-05 20:58:09 +0000

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PeopleComponent } from './people/people.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PeopleComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
